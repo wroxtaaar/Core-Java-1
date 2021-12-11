d9ea496cb6b0d629ee382e2897240ec88ea17262
Yakshit Jain <yakshitjain@Yakshits-MacBook-Pro.local>
2021-12-11 22:49:21 +0530

    Session 3

---
+++ session3/src/main/java/com/crio/session3/activity1/Student.java
@@ -0,0 +1,24 @@
+package com.crio.session3.activity1;
+
+public class Student {
+
+    private String firstName; // mandatory
+    private String lastName; // mandatory
+    private String age; // optional
+    private String gender; // optional
+    private boolean isGraduate; // optional
+    private boolean hasExperience; // optional
+    private String city; // optional
+    private String state; // optional
+    private boolean isEarning; // optional
+
+   
+
+    public static void main(String args[])
+    { 
+        
+        //System.out.println(student);
+     }
+
+}
+
