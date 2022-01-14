a3a0395c00ae78b1db28de52f6f12c0ed255b98f
Yakshit Jain <yakshitjain@Yakshits-MacBook-Pro.local>
2022-01-14 13:31:58 +0530

    Quotes update

--- session2/src/main/java/com/crio/session2/activity2/Quote.java
+++ session2/src/main/java/com/crio/session2/activity2/Quote.java
@@ -1,37 +1,45 @@
 package com.crio.session2.activity2;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Quote {
 
-  private String type;
-  private Value value;
-
-  public Quote() {
+  @JsonProperty("_id")
+  private String id;
+  private List<String> tags;
+  private String author;
+  private String content;
+  
+  public String getId() {
+    return id;
   }
-
-  public String getType() {
-    return type;
+  public void setId(String id) {
+    this.id = id;
   }
-
-  public void setType(String type) {
-    this.type = type;
+  public List<String> getTags() {
+    return tags;
   }
-
-  public Value getValue() {
-    return value;
+  public void setTags(List<String> tags) {
+    this.tags = tags;
   }
-
-  public void setValue(Value value) {
-    this.value = value;
+  public String getAuthor() {
+    return author;
   }
-
+  public void setAuthor(String author) {
+    this.author = author;
+  }
+  public String getContent() {
+    return content;
+  }
+  public void setContent(String content) {
+    this.content = content;
+  }
   @Override
   public String toString() {
-    return "Quote{" +
-        "type='" + type + '\'' +
-        ", value=" + value +
-        '}';
+    return "Quote [author=" + author + ", content=" + content + ", id=" + id + ", tags=" + tags + "]";
   }
 }
--- session2/src/main/java/com/crio/session2/activity2/QuoteFetcher.java
+++ session2/src/main/java/com/crio/session2/activity2/QuoteFetcher.java
@@ -11,20 +11,20 @@ public class QuoteFetcher {
     private static final Logger log = LoggerFactory.getLogger(QuoteFetcher.class);
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplate(); 
-        Quote response = restTemplate.getForObject("https://quoters.apps.pcfone.io/api/random", Quote.class);
+        Quote response = restTemplate.getForObject("https://api.quotable.io/random", Quote.class);
         log.info("==== RESTful API Response using Spring RESTTemplate START =======");
         log.info(response.toString());
         log.info("==== RESTful API Response using Spring RESTTemplate END ======="); 
 
-        // Fetch list of Quotes
-        Quote[] responseQuotes = restTemplate.getForObject("https://quoters.apps.pcfone.io/api/", Quote[].class);
+        // // Fetch list of Quotes
+        QuotesList responseQuotes = restTemplate.getForObject("https://api.quotable.io/quotes", QuotesList.class);
         log.info("==== RESTful API Response using Spring RESTTemplate START =======");
-        log.info(Arrays.asList(responseQuotes).toString());
-        log.info("==== RESTful API Response using Spring RESTTemplate END ======="); 
+        log.info(responseQuotes.toString());
+        log.info("==== RESTful API Response using Spring RESTTemplate END =======");
 
 
-         // Fetch Quote with id 3
-         Quote responseQuote3 = restTemplate.getForObject("https://quoters.apps.pcfone.io/api/{id}", Quote.class,3);
+         // Fetch Quote with id "QdK00IhCNX"
+         Quote responseQuote3 = restTemplate.getForObject("https://api.quotable.io/quotes/{id}", Quote.class,"QdK00IhCNX");
          log.info("==== RESTful API Response using Spring RESTTemplate START =======");
          log.info(Arrays.asList(responseQuote3).toString());
          log.info("==== RESTful API Response using Spring RESTTemplate END ======="); 
