9283303b25f749a7cc42434beba5b33587e0dbdb
Yakshit Jain <yakshitjain@Yakshits-MacBook-Pro.local>
2022-04-17 06:56:30 +0530

    Add File handling as a pre-work

---
+++ prework/src/main/java/com/crio/prework/activity1/FileCount.java
@@ -0,0 +1,13 @@
+package com.crio.prework.activity1;
+
+public class FileCount {
+    //TODO
+    // Read the contents of file using Files.newBufferedReader
+    // Complete the calculation logic for lineCount, wordCount, characterCount, ParagraphCount.
+    // Write the calculate output using Files.newBufferedWriter in the following format in output.txt.
+    //Total Lines Count - 10
+    //Total Words Count - 10
+    //Total Characters Count - 10
+    //Total Paragraphs Count - 10
+    
+}
---
+++ prework/src/main/java/com/crio/prework/activity2/TotalPrice.java
@@ -0,0 +1,10 @@
+package com.crio.prework.activity2;
+
+public class TotalPrice {
+    //TODO
+    // Read all the contents of file using Files.readAllLines
+    // create new arraylist for storing lines to be written to file
+    // Iterate over lines, Split each line by comma, make calcluations and then add line to the newly created arraylist.
+    // Write the calculated output lines present in the arraylist using Files.write in the following format in sample1.csv
+    // ITEM, DESCRIPTION,TOTAL_PRICE
+}
